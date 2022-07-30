de19f9e59685ff7e6862d108a43784b1cb1d154e
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2022-07-30 08:28:24 -0400

    Update ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,43 +7,16 @@ const ExpenseForm = (props) => {
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
-    // ussing state once to change multiple values
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     eneteredAmount: '',
-    //     eneteredDate: ''
-    // });
-
     const titleChangeHandler = (event) => {
-        // getting the title inputted by user and setting to title
-        // i.e event.target.value will get the onChange value from 
-        // this input field where the function is being called
         setEnteredTitle(event.target.value);  
-        // must also set value of other 2 key or they would get lost
-        // setUserInput((prevState) => {
-        //     return {
-        //         ...prevState,
-        //         enteredTitle: event.target.value
-        //     };
-        // });
     };
 
     const amountChangeHandler = (event) => {
-        // setting amount to new input value
         setEnteredAmount(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredAmount: event.target.value
-        // });
     };
 
     const dateChangeHandler = (event) => {
-        // setting date to new input value
         setEnteredDate(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredDate: event.target.value
-        // });
     };
 
     const submitHandler = (event) => {
@@ -52,24 +25,14 @@ const ExpenseForm = (props) => {
         // reloaded/refreshed when form is submitted)
         event.preventDefault();
 
-        // creating an object and setting its values to current
-        // valus of each state
         const expenseData = {
             title: enteredTitle,
             amount: parseFloat(enteredAmount),
             date: new Date(enteredDate)
         };
 
-        // Calling a function from NewExpense.js that will save 
-        // the expense data in that component to be able to 
-        // send and save the expense data in App.js
-        //
-        // This is and example of Child-to-Parent component
-        // communication (Bottom-up)
         props.onSaveExpenseData(expenseData);
 
-        // setting all the states back to empty, as this function
-        // is executed once the form is submited
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
