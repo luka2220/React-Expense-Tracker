7d8bc4b4ea94c642360d890cd8b195f54d54fb97
luka2220 <piplicaluka64@gmail.com>
2022-07-30 08:15:44 -0400

    Completed propject

--- src/App.js
+++ src/App.js
@@ -2,36 +2,10 @@ import React, { useState } from 'react';
 
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
-
-const INITIAL_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: 'e2', 
-    title: 'New TV', 
-    amount: 799.49, 
-    date: new Date(2021, 2, 12) 
-  },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+import './App.css';
 
 const App = () => {
-  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+  const [expenses, setExpenses] = useState([]);
 
   const addExpenseHandler = (expense) => {
     setExpenses(prevExpenses => {
@@ -43,6 +17,7 @@ const App = () => {
 
   return (
     <div>
+      <h1 className="title">Expenses Tracker</h1>
       <NewExpense addExpense={addExpenseHandler}/>
       <Expenses
         items={expenses}
